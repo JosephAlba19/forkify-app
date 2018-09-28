a24f1b0882264e31c9cf9818ecad5fd3db9cb7bb
Joseph Alba <jalba19@gmail.com>
2018-09-28 11:32:47 -0400

    built shopping list controller

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -15,10 +15,10 @@ export const renderItem = item => {
                     </button>
                 </li>
     `;
-    elements.shoping.insertAdjacent('beforeend', markup);
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
 export const deleteItem = id => {
-    const item = document.querySelector('[date-itemid="${id}"]);
-    item.parentElement.removeChild(item);
+    const item = document.querySelector(`[data-itemid="${id}"]`);
+    if (item) item.parentElement.removeChild(item);
 };
